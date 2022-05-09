89104ded9b28afbd806c5745958c10a2d30ced3d
Zenice90 <theohenriksson90@gmail.com>
2022-05-09 18:20:48 +0200

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,4 +1,61 @@
-<template></template>
+<template>
+<!-- Remove the container if you want to extend the Footer to full width. -->
+<div class="container my-5">
+
+    <footer style="background-color: #deded5;">
+    <div class="container p-4">
+      <div class="row">
+        <div class="col-lg-6 col-md-12 mb-4">
+          <h5 class="mb-3" style="letter-spacing: 2px; color: #818963;">ClownTown</h5>
+          <p>
+            En mindre resturang med ett stort hjärta och måltider som passar alla människor. Allt från Vuxna, par och till små barn, har vi måltider för alla. Vi är en familje restaurang där alla människor är våra medlemar. Bli en av oss idag!
+          </p>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4">
+          <h5 class="mb-3" style="letter-spacing: 2px; color: #818963;">Länkar</h5>
+          <ul class="list-unstyled mb-0">
+            <li class="mb-1">
+              <a href="#!" style="color: #4f4f4f;">Vanliga Frågor</a>
+            </li>
+            <li class="mb-1">
+              <a href="#!" style="color: #4f4f4f;">Reservering</a>
+            </li>
+            <li class="mb-1">
+              <a href="#!" style="color: #4f4f4f;">Menyer</a>
+            </li>
+            <li>
+              <a href="#!" style="color: #4f4f4f;">Kontakta oss</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4">
+          <h5 class="mb-1" style="letter-spacing: 2px; color: #818963;">Öppen Tider</h5>
+          <table class="table" style="color: #4f4f4f; border-color: #666;">
+            <tbody>
+              <tr>
+                <td>Mån - fre:</td>
+                <td>08:00 - 20:00</td>
+              </tr>
+              <tr>
+                <td>Lör - Sön:</td>
+                <td>14:00 - 02:00</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+    <div class="text-center p-3" style="background-color: rgba(100, 150, 0, 0.2);">
+      © 2022 Copyright:
+      <a class="text-dark" href="https://mdbootstrap.com/">Grupp 3</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  
+  
+</div>
+<!-- End of .container -->
+</template>
 
 <script>
 export default {
